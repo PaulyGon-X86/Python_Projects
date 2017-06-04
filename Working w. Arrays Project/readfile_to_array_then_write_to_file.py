18685e5ce3bbdc6a5bb83f49ee457cc39a84a581
PaulyGon-X86 <paulericgonzales@gmail.com>
2017-06-04 13:34:37 -0500

    Updated functions

--- Working w. Arrays Project/readfile_to_array_then_write_to_file.py
+++ Working w. Arrays Project/readfile_to_array_then_write_to_file.py
@@ -1,6 +1,25 @@
+#!/usr/bin/env python2
 #2017-06-04
 #Reading a text file into an array, processing it, then writing to a file
-def read():
+def read(f, age):
+    age = f.readline()
+    return(age)
+
 def process():
+    pass
+    return
+
 def write():
+    pass
+    return
+
 def main():
+    age_list = []
+    file_handler = open('#path to file', 'r')
+    age_list = read(file_handler, age_list)
+    process()
+    write()
+    return
+
+if __name__ == "__main__":
+    main()
