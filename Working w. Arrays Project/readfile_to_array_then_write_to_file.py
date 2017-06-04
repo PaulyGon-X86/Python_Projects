b6d8afeb636055933e188e4103d7b451ed2a045b
PaulyGon-X86 <paulericgonzales@gmail.com>
2017-06-04 16:11:49 -0500

    Testing sort function

--- Working w. Arrays Project/readfile_to_array_then_write_to_file.py
+++ Working w. Arrays Project/readfile_to_array_then_write_to_file.py
@@ -2,23 +2,29 @@
 #2017-06-04
 #Reading a text file into an array, processing it, then writing to a file
 def read(f, age):
-    age = f.readline()
+    age = f.readlines()
+    print(age, '#2')
     return(age)
 
-def process():
-    pass
-    return
+def process(age):
+    age.sort()
+    print(age, '#4')
+    return(age)
 
-def write():
-    pass
-    return
+#def write(age):
+#    for element in age:
+#    print(element) 
+#    return
 
 def main():
     age_list = []
-    file_handler = open('#path to file', 'r')
+    print(age_list, '#1')
+    file_handler = open('agelist.txt', 'r')
     age_list = read(file_handler, age_list)
-    process()
-    write()
+    print(age_list, '#3')
+    age_list = process(age_list)
+    print(age_list, '#5')
+#    write(age_list)
     return
 
 if __name__ == "__main__":
